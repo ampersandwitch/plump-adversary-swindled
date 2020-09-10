c30ffcd81db50f15626684b77333887d778720a6
Tiffany Valentine <52210944+devWitch-ctrl@users.noreply.github.com>
2020-09-10 15:32:08 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'JXsNygd5hFS7hNpGcm9Sdh8Dq',
+  consumer_secret:      'jDAlbwHZPSrP62xOhGVk9r2H0EWKtfulvKRz9iPqHG5KxatGtP',
+  access_token:         '1301947055376482304-3SnM59OlAuqHkeHxaAVLXI9d7CXGkV',
+  access_token_secret:  'bUClR5UlWK94oNJzBo5aVVmBDYJ4hld69UQEpy1xb5Ttr'
+}
